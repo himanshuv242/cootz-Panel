d35a6666fd904ed65606764c3cd94d351bcdb02e
himanshuv242 <himanshuv242cp@gmail.com>
2022-02-01 02:47:42 +0530

    API data fetched and shown in DROPDOWN

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import DropdownComponent from "./components/DropdownComponent";
 
 function App() {
-
   var li_elements = document.querySelectorAll(".wrapper_left ul li");
   var item_elements = document.querySelectorAll(".item");
   for (var i = 0; i < li_elements.length; i++) {
@@ -29,11 +29,11 @@ function App() {
     });
   }
 
-  const [activeLi,setActiveLi]=useState("")
-  const liValue=(e)=>{
-    setActiveLi(e.target.textContent)
+  const [activeLi, setActiveLi] = useState("");
+  const liValue = (e) => {
+    setActiveLi(e.target.textContent);
     // console.log(activeLi);
-  }
+  };
 
   return (
     <div className="wrapper">
@@ -45,7 +45,7 @@ function App() {
           <li data-li="nodejs" className="active" onClick={liValue}>
             <p>Create contest</p>
           </li>
-          <li data-li="reactjs" onClick={liValue} >
+          <li data-li="reactjs" onClick={liValue}>
             <p>Add Question to contest</p>
           </li>
           <li data-li="vuejs" onClick={liValue}>
@@ -60,37 +60,76 @@ function App() {
               <div className="img"></div>
               <p>Please be sure of the excel form-data</p>
             </div>
-            <form id="uploadForm" encType="multipart/form-data" action="/readxl" method="post">
+            <form
+              id="uploadForm"
+              encType="multipart/form-data"
+              action="/readxl"
+              method="post"
+            >
               <input type="file" name="userPhoto" />
               <input type="submit" value="submit" name="submit" />
             </form>
-
           </div>
-          <div className="item nodejs" style={{'marginTop':'850px'}}>
+          <div className="item nodejs" style={{ marginTop: "850px" }}>
             <div className="item_info">
               {/* <!-- <div className="img"></div> --> */}
               <div className="form_container">
-                <form action="/public/js/connetToBackend.js" method="post" className="form">
-                  <label htmlFor="contest_name">CONTEST NAME</label><br />
-                  <input type="text" id="contest_name" name="contest_name" placeholder="EG : UPSC" />
+                <form
+                  action="/public/js/connetToBackend.js"
+                  method="post"
+                  className="form"
+                >
+                  <label htmlFor="contest_name">CONTEST NAME</label>
+                  <br />
+                  <input
+                    type="text"
+                    id="contest_name"
+                    name="contest_name"
+                    placeholder="EG : UPSC"
+                  />
 
                   <label htmlFor="contest_fees">CONTEST FEES</label>
-                  <input type="number" id="contest_fees" name="contest_fees" placeholder="Rs 100" />
+                  <input
+                    type="number"
+                    id="contest_fees"
+                    name="contest_fees"
+                    placeholder="Rs 100"
+                  />
 
-                  <label htmlFor="pool_size">POOL SIZE</label><br />
-                  <input type="number" id="pool_size" name="pool_size" placeholder="100" />
+                  <label htmlFor="pool_size">POOL SIZE</label>
+                  <br />
+                  <input
+                    type="number"
+                    id="pool_size"
+                    name="pool_size"
+                    placeholder="100"
+                  />
 
-                  <label htmlFor="winning_amount">WINNING AMOUNT</label><br />
-                  <input type="number" id="winning_amount" name="winning_amount" placeholder="Rs 10000" />
+                  <label htmlFor="winning_amount">WINNING AMOUNT</label>
+                  <br />
+                  <input
+                    type="number"
+                    id="winning_amount"
+                    name="winning_amount"
+                    placeholder="Rs 10000"
+                  />
 
-                  <label htmlFor="subject">SUBJECT</label><br />
-                  <input type="text" id="subject" name="subject" placeholder="MATHS" />
+                  <label htmlFor="subject">SUBJECT</label>
+                  <br />
+                  <input
+                    type="text"
+                    id="subject"
+                    name="subject"
+                    placeholder="MATHS"
+                  />
 
-                  <label htmlFor="start_date">START DATE</label><br />
+                  <label htmlFor="start_date">START DATE</label>
+                  <br />
                   <input type="date" id="start_date" name="start_date" />
                   <input type="time" id="start_date" name="start_date" />
 
-                  <label htmlFor="end_date">END DATE</label><br />
+                  <label htmlFor="end_date">END DATE</label>
+                  <br />
                   <input type="date" id="end_date" name="end_date" />
 
                   <label htmlFor="start_time">START TIME</label>
@@ -99,48 +138,67 @@ function App() {
                   <label htmlFor="end_time">END TIME</label>
                   <input type="time" id="end_time" name="end_time" />
 
-                  <label htmlFor="contest_duration">DURATION OF CONTEST ( in mins )</label>
-                  <input type="time" id="contest_duration" name="contest_duration" />
-                  <label htmlFor="sections">NO OF SECTIONS</label><br />
-                  <input type="number" id="sections" name="sections" placeholder="2" />
+                  <label htmlFor="contest_duration">
+                    DURATION OF CONTEST ( in mins )
+                  </label>
+                  <input
+                    type="time"
+                    id="contest_duration"
+                    name="contest_duration"
+                  />
+                  <label htmlFor="sections">NO OF SECTIONS</label>
+                  <br />
+                  <input
+                    type="number"
+                    id="sections"
+                    name="sections"
+                    placeholder="2"
+                  />
 
-                  <label htmlFor="questions">NO OF QUESTIONS ( IN EACH SECTION )</label><br />
-                  <input type="number" id="questions" name="questions" placeholder="20" />
-                  
-                  <label htmlFor="contest_image">UPLOAD IMAGE OF CONTEST</label><br />
+                  <label htmlFor="questions">
+                    NO OF QUESTIONS ( IN EACH SECTION )
+                  </label>
+                  <br />
+                  <input
+                    type="number"
+                    id="questions"
+                    name="questions"
+                    placeholder="20"
+                  />
+
+                  <label htmlFor="contest_image">UPLOAD IMAGE OF CONTEST</label>
+                  <br />
                   <input type="file" name="userPhoto" />
 
                   <input type="submit" value="CREATE" />
                 </form>
               </div>
             </div>
-
           </div>
-          <div className="item reactjs" style={{'display': 'none'}}>
-            <div className="item_info">
-              <div className="img"></div>
-
-            </div>
-
+          <div className="item reactjs" style={{ display: "none" }}>
+            
+              <DropdownComponent/>
+            
           </div>
-          <div className="item vuejs" style={{'display': 'none'}}>
+          <div className="item vuejs" style={{ display: "none" }}>
             <div className="item_info">
               <div className="img"></div>
               <p>vue.js</p>
             </div>
-            <p>Vue is a progressive framework for building user interfaces. Unlike other monolithic
-              frameworks, Vue
-              is designed from the ground up to be incrementally adoptable. The core library is focused on
-              the
-              view layer only, and is easy to pick up and integrate
-              with other libraries or existing projects. On the other hand, Vue is also perfectly capable
-              of
-              powering sophisticated Single-Page Applications when used in combination with modern tooling
-              and
-              supporting libraries.</p>
-            <p>If you’d like to learn more about Vue before diving in, we created a video walking through
-              the core
-              principles and a sample project.</p>
+            <p>
+              Vue is a progressive framework for building user interfaces.
+              Unlike other monolithic frameworks, Vue is designed from the
+              ground up to be incrementally adoptable. The core library is
+              focused on the view layer only, and is easy to pick up and
+              integrate with other libraries or existing projects. On the other
+              hand, Vue is also perfectly capable of powering sophisticated
+              Single-Page Applications when used in combination with modern
+              tooling and supporting libraries.
+            </p>
+            <p>
+              If you’d like to learn more about Vue before diving in, we created
+              a video walking through the core principles and a sample project.
+            </p>
           </div>
         </div>
       </div>
---
+++ src/components/DropdownComponent.js
@@ -0,0 +1,44 @@
+import React,{useState,useEffect} from 'react';
+import Dropdown from 'react-bootstrap/Dropdown';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const DropdownComponent = () => {
+
+  const [item,setItem]= useState([]);
+
+  const fetchData = () => {
+    fetch("http://cootz-backend-api.herokuapp.com/getallcontests").then((response) => {
+    return response.json();
+  }).then((data) => {
+    // console.log(data);
+    setItem(data)
+  })
+  }
+  
+  useEffect(() => {
+    fetchData();
+  },[])
+
+
+
+  return (
+    <Dropdown>
+    <Dropdown.Toggle variant="success" id="dropdown-basic">
+      SELECT CONTEST TYPE
+    </Dropdown.Toggle>
+  
+    <Dropdown.Menu>
+      {/* <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+      <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+      <Dropdown.Item href="#/action-3">Something else</Dropdown.Item> */}
+
+      {item.map(data =>(
+        <Dropdown.Item  eventKey={data._id.toString()} >{data.constestType} : {data.contestsubType}</Dropdown.Item>
+      ))}
+    </Dropdown.Menu>
+  </Dropdown>
+  )
+};
+
+export default DropdownComponent;
