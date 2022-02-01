e429ae2be62ac7e439faa156b0e5dddb855a1474
himanshuv242 <himanshuv242cp@gmail.com>
2022-02-01 13:03:49 +0530

    Dropdown data changes while options selected

--- src/components/DropdownComponent.js
+++ src/components/DropdownComponent.js
@@ -1,46 +1,77 @@
-import React,{useState,useEffect} from 'react';
-import Dropdown from 'react-bootstrap/Dropdown';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import React, { useState, useEffect } from "react";
+import Dropdown from "react-bootstrap/Dropdown";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Table from 'react-bootstrap/Table'
 
 const DropdownComponent = () => {
+  const [item, setItem] = useState([]);
 
-  const [item,setItem]= useState([]);
+  const [indivisual, setIndivisual] = useState();
 
   const fetchData = () => {
-    fetch("http://cootz-backend-api.herokuapp.com/getallcontests").then((response) => {
-    return response.json();
-  }).then((data) => {
-    // console.log(data);
-    setItem(data)
-  })
-  }
-  
+    fetch("http://cootz-backend-api.herokuapp.com/getallcontests")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        // console.log(data);
+        setItem(data);
+      });
+  };
+
   useEffect(() => {
     fetchData();
-  },[])
-
-  console.log(item[0]);
-
-
+  }, []);
 
   return (
-    <Dropdown>
-    <Dropdown.Toggle variant="success" id="dropdown-basic">
-      SELECT CONTEST TYPE
-    </Dropdown.Toggle>
-  
-    <Dropdown.Menu>
-      {/* <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-      <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-      <Dropdown.Item href="#/action-3">Something else</Dropdown.Item> */}
+    <>
+      <Dropdown>
+        <Dropdown.Toggle variant="success" id="dropdown-basic">
+          SELECT CONTEST TYPE
+        </Dropdown.Toggle>
 
-      {item.map(data =>(
-        <Dropdown.Item  eventKey={data._id.toString()} onClick={() => console.log(data._id.toString())} >{data.constestType} : {data.contestsubType}</Dropdown.Item>
-      ))}
-    </Dropdown.Menu>
-  </Dropdown>
-  )
+        <Dropdown.Menu>
+          {item.map((bata) => {
+            return (
+              <Dropdown.Item
+                key={bata._id}
+                onClick={() => setIndivisual(bata._id)}
+              >
+                {bata.constestType} : {bata.contestsubType}
+              </Dropdown.Item>
+            );
+          })}
+        </Dropdown.Menu>
+      </Dropdown>
+      <div className="container my-4">
+      {item.map((bata) => {
+        if (bata._id === indivisual) {
+          return (
+            <Table striped bordered hover variant="dark">
+  <tbody>
+    <tr>
+      <td>Id</td>
+      <td>{bata._id}</td>
+      
+    </tr>
+    <tr>
+      <td>Created at</td>
+      <td>{bata.createdAt}</td>
+      
+    </tr>
+    <tr>
+      <td>Updated at</td>
+      <td colSpan={2}>{bata.updatedAt}</td>
+      
+    </tr>
+  </tbody>
+</Table>
+          );
+        }
+      })}
+      </div>
+    </>
+  );
 };
 
 export default DropdownComponent;
